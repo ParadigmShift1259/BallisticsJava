7cdb21ebe6463fd0ea0e225ad6b77ec6506fd984
Scott Coursin <scott.coursin@gmail.com>
2026-02-04 20:35:41 -0600

    Update constants to 2026, need to use baseUnitMagnitude()

--- Ballistics.java
+++ Ballistics.java
@@ -1,44 +1,44 @@
 import static edu.wpi.first.units.Units.*; // library for units, (e.g. MetersPerSecondPerSecond)
 
-import javax.lang.model.util.ElementScanner14;
+//import javax.lang.model.util.ElementScanner14;
 
-import edu.wpi.first.units.VelocityUnit;
+//import edu.wpi.first.units.VelocityUnit;
 import edu.wpi.first.units.measure.*; // library for types (e.g. LinearAcceleration)
 
 public class Ballistics {
-    
     private static final LinearAcceleration gravity = MetersPerSecondPerSecond.of(9.81);
-    private static final Mass flywheelMass = Pound.of(2.8);
+    private static final Mass flywheelMass = Pound.of(1.5);
     
     private static final Distance flywheelRadius = Inch.of(2.0);
     private static final double flywheelRotInertiaFrac = 1.0 / 2.0; // used to be scalar if this causes issues
+    //private static final double flywheelRotInertiaFrac = 0.6659;  // based on the SDS brass hollow flywheel with MOI 4 [pound][square inches]
+    //private static final MomentOfInertia flywheelRotInertia = KilogramSquareMeters.of(flywheelMass.in(Kilogram) * flywheelRotInertiaFrac * flywheelRadius.in(Meter) * flywheelRadius.in(Meter));
 
-    private static final MomentOfInertia flywheelRotInertia = KilogramSquareMeters.of(flywheelMass.in(Kilogram) * flywheelRotInertiaFrac * flywheelRadius.in(Meter) * flywheelRadius.in(Meter));
+    private static final Mass fuelMass = Pound.of(0.5);
+    private static final Distance fuelRadius = Inch.of(5.91 / 2);
+    //private static final double fuelRotInertiaFrac = 2.0 / 3.0;  // 2/3 Mr^2 hollow sphere
+    private static final double fuelRotInertiaFrac = 2.0 / 5.0;  // 2/5 Mr^2 solid sphere
+    //private static final MomentOfInertia fuelRotInertia = fuelRotInertiaFrac * fuelMass * fuelRadius * fuelRadius;
 
-    private static final Mass cargoMass = Ounce.of(9.5);
-    private static final Distance cargoRadius = Inch.of(4.75);
-    private static final double cargoRotInertiaFrac = 2.0 / 3.0; // used to be scalar
-    private static final MomentOfInertia cargoRotInertia = KilogramSquareMeters.of(cargoRotInertiaFrac * cargoMass.in(Kilogram) * cargoRadius.in(Meter) * cargoRadius.in(Meter));
+    private static final double massRatio = flywheelMass.in(Kilogram) / fuelMass.in(Kilogram);
+    //private static final double rotInertiaRatio = flywheelRotInertia.in(KilogramSquareMeters) / fuelRotInertia.in(KilogramSquareMeters);
 
-    private static final double massRatio = flywheelMass.in(Kilogram) / cargoMass.in(Kilogram);
-    private static final double rotInertiaRatio = flywheelRotInertia.in(KilogramSquareMeters) / cargoRotInertia.in(KilogramSquareMeters);
+    private static final Angle maxAngle = Degree.of(80.0);
+    private static final Angle minAngle = Degree.of(20.3);
 
-    private static final Angle maxAngle = Degree.of(60.0);
-    private static final Angle minAngle = Degree.of(33.3);
-
-    private static final Distance robotHeight = Foot.of(3.0);
+    //private static final Distance robotHeight = Foot.of(3.0);
     private static final Distance defaultTargetDist = Foot.of(2.5);
 
-    private static final Distance defaultTargetHeight = Foot.of(80.0);
-    private static final Distance defaultHeightAboveHub = Foot.of(9.2);
+    //private static final Distance defaultTargetHeight = Foot.of(80.0);
+    private static final Distance defaultHeightAboveHub = Foot.of(6.0).plus(Inch.of(6.0));
 
     private Time _timeOne = Seconds.of(0.0);
     private Time _timeTwo = Seconds.of(0.0);
     private Time _timeTotal = Seconds.of(0.0);
 
     private Distance _heightAboveHub = defaultHeightAboveHub;
-    private Distance _heightRobot = Foot.of(3.0);
-    private Distance _heightTarget = Foot.of(8.67);
+    private Distance _heightRobot = Inch.of(25.0);
+    private Distance _heightTarget = Foot.of(6.0).minus(Inch.of(4.0));
     private Distance _heightMax = Meter.of(16.0);
 
     private Distance _xInput = Meter.of(0.0);
@@ -57,24 +57,25 @@ public class Ballistics {
     boolean _bClampAngle = true;
 
     public Ballistics() {
-        //System.out.print("Hello.");
-        _heightRobot = Foot.of(3.0);
-        _heightTarget = Foot.of(8.67);
     }
 
     Distance HubHeightToMaxHeight()
     {
-        double t1 = (_heightTarget.minus(_heightRobot)).magnitude();
-        double t2 = _xInput.magnitude() + _xTarget.magnitude();
-        double t3 = _heightAboveHub.magnitude() - _heightRobot.magnitude();
-        double t4 = _xTarget.magnitude() * _xInput.magnitude() * (_xInput.magnitude() + _xTarget.magnitude());
+        double xInput = _xInput.magnitude();
+        double hTarg = (_heightTarget.minus(_heightRobot)).magnitude();
+        double dist = xInput + _xTarget.magnitude();
+        double hAbove = _heightAboveHub.magnitude() - _heightRobot.magnitude();
+        double x = _xTarget.magnitude() * xInput * (dist);
+
+//        double aValue = (xInput * hTarg - dist * hAbove) / x;
+//        double bValue = (dist * dist * hAbove - xInput * xInput * hTarg) / x;
 
         double aValue = (_xInput.magnitude() * (_heightTarget.magnitude() - _heightRobot.magnitude()) - (_xInput.magnitude() + _xTarget.magnitude()) * (_heightAboveHub.magnitude() - _heightRobot.magnitude())) / (_xTarget.magnitude() * _xInput.magnitude() * (_xInput.magnitude() + _xTarget.magnitude()));
         double bValue = ((_xInput.magnitude() + _xTarget.magnitude()) * (_xInput.magnitude() + _xTarget.magnitude()) * (_heightAboveHub.magnitude() - _heightRobot.magnitude()) - _xInput.magnitude() * _xInput.magnitude() * (_heightTarget.magnitude() - _heightRobot.magnitude())) / (_xTarget.magnitude() * _xInput.magnitude() * (_xInput.magnitude() + _xTarget.magnitude()));
 
         _heightMax = Meter.of(-1.0 * bValue * bValue / (4.0 * aValue) + _heightRobot.magnitude());
 
-        // frc::SmartDashboard::PutNumber("HubHeightToMaxHeight", units::foot_t(_heightMax).to<double>());
+        System.out.println("max height " + _heightMax.magnitude());
 
         return _heightMax;
     }
@@ -82,6 +83,10 @@ public class Ballistics {
     Time CalcTimeOne()
     {
         // C++ Code: _timeOne = math::sqrt(2.0 * (_heightMax - _heightRobot) / gravity);
+        double hMax = _heightMax.baseUnitMagnitude();
+        double hBot = _heightRobot.baseUnitMagnitude();
+        double h = hMax - hBot;
+        double t1 = Math.sqrt(2.0 * h / gravity.magnitude());
         _timeOne = Seconds.of(Math.sqrt(2.0 * (_heightMax.magnitude() - _heightRobot.magnitude()) / gravity.magnitude()));
         return _timeOne;
     }
@@ -101,24 +106,11 @@ public class Ballistics {
      * 
         m_timeTotal = CalcTimeOne() + CalcTimeTwo();
 
-        // Estimate the landing angle
-        // final vy = v0 - gt
-        meters_per_second_t vyfinal = m_velYInit - gravity * m_timeTotal;
-        meters_per_second_t vxfinal = m_velXInit; // No drag
-        radian_t beta = units::math::atan(vyfinal / vxfinal);
-        m_landingAngle = beta;
-        qDebug("m_landingAngle %.3f CalcTotalTime()", m_landingAngle);
-
         return m_timeTotal;
      */
 
         _timeTotal = CalcTimeOne().plus(CalcTimeTwo());
 
-        // TODO: Why is this calculation in this method? Surely it should be its own method, as calculating the landing angle is a byproduct of this method
-        LinearVelocity vyfinal = MetersPerSecond.of(_velYInit.magnitude() - gravity.magnitude() * _timeTotal.magnitude());
-        LinearVelocity vxfinal = _velXInit; // No drag
-        Angle beta = Radian.of(Math.atan(vyfinal.magnitude() / vxfinal.magnitude()));
-        _landingAngle = beta;
         return _timeTotal;
     }
 
@@ -163,12 +155,20 @@ public class Ballistics {
     
         _angleInit = Radian.of(Math.atan(_velYInit.magnitude() / _velXInit.magnitude()));   
 
-        if (_bClampAngle)
+        if (_bClampAngle && minAngle.magnitude() < maxAngle.magnitude())
         {
             _angleInit = Degree.of(clamp(_angleInit.magnitude(), minAngle.magnitude(), maxAngle.magnitude()));
+            // If we clamp the angle, we need to recalc the vx and vy as inputs to CalcInitVelWithAngle()
+            _velYInit = MetersPerSecond.of(_velInit.magnitude() * Math.sin(_angleInit.magnitude()));
+            _velXInit = MetersPerSecond.of(_velInit.magnitude() * Math.cos(_angleInit.magnitude()));
         }
 
         CalcInitVelWithAngle();
+
+        LinearVelocity vyfinal = MetersPerSecond.of(_velYInit.magnitude() - gravity.magnitude() * _timeTotal.magnitude());
+        LinearVelocity vxfinal = _velXInit; // No drag
+        Angle beta = Radian.of(Math.atan(vyfinal.magnitude() / vxfinal.magnitude()));
+        _landingAngle = beta;
 
         return _velInit;
     }
@@ -193,16 +193,25 @@ public class Ballistics {
         return _velInit;
     }
 
-    Angle GetInitAngle()
+    Angle getInitAngle()
     {
         return _angleInit;
     }
 
+    Angle getLandingAngle()
+    {
+        return _landingAngle;
+    }
+
+    LinearVelocity getInitVelocity() {
+        return _velInit;
+    }
+
     AngularVelocity CalcInitRPMs(  Distance distance        // Floor distance (to front of cone?)
-                                                        , Distance targetDist      // Target distance within cone
-                                                        , Distance heightAboveHub  // Hub Height to max height
-                                                        , Distance targetHeight    // Height at end point within cone
-                                                    )
+                                 , Distance targetDist      // Target distance within cone
+                                 , Distance heightAboveHub  // Hub Height to max height
+                                 , Distance targetHeight    // Height at end point within cone
+                                )
     {
         _xInput = distance;
         _xTarget = targetDist;
@@ -220,7 +229,7 @@ public class Ballistics {
         // C++ Code:
         // m_rotVelInit = radian_t(1.0) * m_velInit / m_flywheelRadius * (2.0 + (fuelRotInertiaFrac + 1.0) / (flywheelRotInertiaFrac * m_massRatio));
         // *** TODO: I AM REALLY UNSURE IF THIS CONVERSION IS CORRECT IN TERMS OF THE MATH. NEED TO DOUBLE CHECK
-        _rotVelInit = RadiansPerSecond.of(Radian.of(1.0).magnitude() * _velInit.magnitude() / flywheelRadius.magnitude() * (2.0 + (cargoRotInertiaFrac + 1.0) / (flywheelRotInertiaFrac * massRatio)));
+        _rotVelInit = RadiansPerSecond.of(Radian.of(1.0).magnitude() * _velInit.magnitude() / flywheelRadius.magnitude() * (2.0 + (fuelRotInertiaFrac + 1.0) / (flywheelRotInertiaFrac * massRatio)));
         _rpmInit = _rotVelInit;
 
         return _rpmInit;
